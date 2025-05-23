5e09b443c02595c85c52340aa4f02ccdd908bccc
Jennifer Adamski <jennifer.adamski37@gmail.com>
2025-05-23 17:56:19 +0200

    Added a test to check the authentication process

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,10 @@ import { defineConfig } from "cypress";
 export default defineConfig({
     e2e: {
         baseUrl: 'http://localhost:3000',
+        env: {
+            // user_email: 'ADD YOUR TEST EMAIL',
+            // user_password: 'ADD YOUR TEST PASSWORD',
+        },
     },
 
     component: {
--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -3,4 +3,15 @@ describe('The Login Page', () => {
         cy.visit('/login');
         cy.get("h1").should("contain", "Se connecter");
     })
+
+    it('logs in a user successfully and redirect it to the dashboard', function () {
+        const user_email = Cypress.env('user_email');
+        const user_password = Cypress.env('user_password');
+
+        cy.visit('/login');
+        cy.get('input[name=email]').type(user_email);
+        cy.get('input[name=password]').type(`${user_password}{enter}`);
+        cy.url().should('include', '/dashboard');
+        cy.get('h1').should('contain', 'Tableau de bord');
+    })
 })
